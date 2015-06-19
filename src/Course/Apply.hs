995eb4f23b07a0146f86af53c9e57b206c565536
miraks <a@vldkn.net>
2015-06-20 00:23:25 +0300

    Apply

--- src/Course/Apply.hs
+++ src/Course/Apply.hs
@@ -36,7 +36,7 @@ instance Apply Id where
     -> Id a
     -> Id b
   (<*>) =
-    error "todo: Course.Apply (<*>)#instance Id"
+    (<$>) . runId
 
 -- | Implement @Apply@ instance for @List@.
 --
@@ -47,8 +47,8 @@ instance Apply List where
     List (a -> b)
     -> List a
     -> List b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance List"
+  (<*>) fs xs =
+    flatMap (<$> xs) fs
 
 -- | Implement @Apply@ instance for @Optional@.
 --
@@ -66,7 +66,7 @@ instance Apply Optional where
     -> Optional a
     -> Optional b
   (<*>) =
-    error "todo: Course.Apply (<*>)#instance Optional"
+    applyOptional
 
 -- | Implement @Apply@ instance for reader.
 --
@@ -89,8 +89,8 @@ instance Apply ((->) t) where
     ((->) t (a -> b))
     -> ((->) t a)
     -> ((->) t b)
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance ((->) t)"
+  ((<*>) f g) x =
+    f x $ g x
 
 -- | Apply a binary function in the environment.
 --
@@ -117,8 +117,8 @@ lift2 ::
   -> f a
   -> f b
   -> f c
-lift2 =
-  error "todo: Course.Apply#lift2"
+lift2 f a b =
+  f <$> a <*> b
 
 -- | Apply a ternary function in the environment.
 --
@@ -149,8 +149,8 @@ lift3 ::
   -> f b
   -> f c
   -> f d
-lift3 =
-  error "todo: Course.Apply#lift2"
+lift3 f a b c =
+  f <$> a <*> b <*> c
 
 -- | Apply a quaternary function in the environment.
 --
@@ -182,8 +182,8 @@ lift4 ::
   -> f c
   -> f d
   -> f e
-lift4 =
-  error "todo: Course.Apply#lift4"
+lift4 f a b c d =
+  f <$> a <*> b <*> c <*> d
 
 -- | Sequence, discarding the value of the first argument.
 -- Pronounced, right apply.
@@ -209,7 +209,7 @@ lift4 =
   -> f b
   -> f b
 (*>) =
-  error "todo: Course.Apply#(*>)"
+  lift2 $ flip const
 
 -- | Sequence, discarding the value of the second argument.
 -- Pronounced, left apply.
@@ -235,7 +235,7 @@ lift4 =
   -> f a
   -> f b
 (<*) =
-  error "todo: Course.Apply#(<*)"
+  lift2 const
 
 -----------------------
 -- SUPPORT LIBRARIES --
